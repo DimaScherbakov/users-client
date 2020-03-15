09b3c3d15436e358decac446113d5876f4ddad8c
Dima Scherbakov <dima.r.scherbakov@gmail.com>
2020-03-15 14:52:55 +0200

    add: base url to app environment, add: methods to get users data from firestore

--- src/app/core/users.service.ts
+++ src/app/core/users.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  usersPerPage(pageIndex: number): Observable<any> {
+    return this.http.get(`${environment.baseUrl}/users/page/${pageIndex}`);
+  }
+
+  pagesCount(): Observable<any> {
+    return this.http.get(`${environment.baseUrl}/users/pages-count`);
+  }
+
+  userStatistics(userId: number): Observable<any> {
+    return this.http.get(`${environment.baseUrl}/user-statistics/${userId}`);
+  }
 }
